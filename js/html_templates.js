d2fad03286bb259e301cd62e105e09c9d33122a7
cartobaldrica <garethfranklin@comcast.net>
2021-07-29 14:59:09 -0500

    update site header

--- js/html_templates.js
+++ js/html_templates.js
@@ -1,59 +1,58 @@
+
+
+var url = window.location.pathname,
+    level1 = ["research","education","people","production"],
+    level2 = ["areas","G370","G572","G575"],
+    level = "";
+
+function determineLevel(){
+    level1.forEach(function(t){
+        let search = url.search(t);
+        if (search > 0){
+            level = "../"
+        }
+    })
+    level2.forEach(function(t){
+        var search = url.search(t);
+        if (search > 0){
+            level = "../../"
+        }
+    })
+}
+determineLevel();
+
 class Header extends HTMLElement {
     constructor() {
       super();
     }
   
     connectedCallback() {
-        /*this.innerHTML = `
-                <nav class="navbar">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span> 
-                            </button>
-                            <a class="navbar-brand" href="../index.html"><img src="../img/UWCL_logo_white.png" alt="UW Cartography Lab" style="width:80px;height:auto;"></a>
-                        </div>
-            
-                        <div class="collapse navbar-collapse navbar-right" id="myNavbar">
-                            <ul class="nav navbar-nav navText">
-                                <li><a href="../production/index.html" id="navText">Production</a></li>
-                                <li><a href="../research/index.html" id="navText">Research</a></li>
-                                <li><a href="../education/index.html" id="navText">Education</a></li>
-                                <li><a href="../people/index.html" id="navText">People</a></li>
-                                <li><a href="../sitemap.html" id="navText">Site Map</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-            `;*/
             this.innerHTML = `
                 <nav class="navbar navbar-expand-md navbar-light">
                     <div class="container-fluid" id="nav-header">
-                        <a class="navbar-brand" href="../index.html"><img src="../img/UWCL_logo_white.png" alt="UW Cartography Lab" style="width:80px;height:auto;"></a>
+                        <a class="navbar-brand" href="` + level +`index.html"><img src="` + level +`img/UWCL_logo_white.png" alt="UW Cartography Lab" style="width:80px;height:auto;"></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse w-100 order-3 dual-collapse2" id="myNavbar">
                             <ul class="navbar-nav ms-auto">
                                 <li class="nav-item">
-                                    <a href="../production/index.html" id="navText">Production</a>
+                                    <a href="` + level +`production/index.html" id="navText">Production</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="../research/index.html" id="navText">Research</a>
+                                    <a href="` + level +`research/index.html" id="navText">Research</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="../education/index.html" id="navText">Education</a>
+                                    <a href="` + level +`education/index.html" id="navText">Education</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="../people/index.html" id="navText">People</a>
+                                    <a href="` + level +`people/index.html" id="navText">People</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="../people/github.html" id="navText">Github</a>
+                                    <a href="` + level +`github.html" id="navText">Github</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="../sitemap.html" id="navText">Site Map</a>
+                                    <a href="` + level +`sitemap.html" id="navText">Site Map</a>
                                 </li>
                             </ul>
                         </div>
@@ -64,49 +63,6 @@ class Header extends HTMLElement {
   }
   
 customElements.define('site-header', Header);
-
-class Header2 extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-        this.innerHTML = `
-            <nav class="navbar navbar-expand-md navbar-light">
-                <div class="container-fluid" id="nav-header">
-                    <a class="navbar-brand" href="../index.html"><img src="../../img/UWCL_logo_white.png" alt="UW Cartography Lab" style="width:80px;height:auto;"></a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse w-100 order-3 dual-collapse2" id="myNavbar">
-                        <ul class="navbar-nav ms-auto">
-                            <li class="nav-item">
-                                <a href="../../production/index.html" id="navText">Production</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../../research/index.html" id="navText">Research</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../../education/index.html" id="navText">Education</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="../../people/index.html" id="navText">People</a>
-                            </li>
-                            <li class="nav-item">
-                                    <a href="../..//github.html" id="navText">Github</a>
-                                </li>
-                            <li class="nav-item">
-                                <a href="../../sitemap.html" id="navText">Site Map</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        `;
-    }
-}
-  
-customElements.define('site-header-2', Header2);
 
 class Footer extends HTMLElement {
     constructor() {
@@ -120,7 +76,7 @@ class Footer extends HTMLElement {
                         <div class="row">
                             <div class="align-center">
                                 <br>
-                                <img src="../img/UWCL_logo_gray.png">
+                                <img src="` + level +`img/UWCL_logo_gray.png">
                                 <p>Copyright 2019&copy; All Rights Reserved.</p>
                             </div>
                         </div>
@@ -131,29 +87,3 @@ class Footer extends HTMLElement {
   }
   
 customElements.define('site-footer', Footer);
-
-class Footer2 extends HTMLElement {
-    constructor() {
-      super();
-    }
-  
-    connectedCallback() {
-        this.innerHTML = `
-                <footer class="footer navbar-fixed-bottom">
-                    <div class="container">
-                        <div class="row">
-                            <div class="align-center">
-                                <br>
-                                <img src="../../img/UWCL_logo_gray.png">
-                                <p>Copyright 2019&copy; All Rights Reserved.</p>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            `;
-        }
-  }
-  
-customElements.define('site-footer-2', Footer2);
-  
-  
